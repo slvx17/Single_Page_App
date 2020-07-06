ff55626db2dfb2338b953586215e13a23d762844
slvx <salva@Lydias-MacBook-Air.local>
2020-07-06 14:01:38 +0700

    added react to app

---
+++ src/todolist.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TodoList extends Component    {
+    render(){
+        return(
+            <h1>Todos App</h1>
+        )
+    }
+}
+
+export default TodoList;
